f008124b0dfe4f9d06e448a596e04d82e74b1368
timedignitorbuilt <Version@2024>
2024-10-01 17:53:59 +0530

    add data

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,13 @@
 import 'dart:async';
 
 import 'package:anything/CommonWidget.dart';
-import 'package:anything/MainScreen/login_screen.dart';
 import 'package:anything/common_color.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 
+import 'ConstantData/Constant_data.dart';
 import 'Home_screen.dart';
+import 'MainScreen/onboardingScreen.dart';
 import 'MainScreen/register_screen.dart';
 
 
@@ -24,9 +25,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home:  MyHomePage(title: '',),
+    return MaterialApp(
+      home:  const MyHomePage(title: '',),
     debugShowCheckedModeBanner: false,
+
+        routes: <String, WidgetBuilder>{
+          '/frame': (BuildContext context) =>  onboardingScreen(),
+          '/registration': (BuildContext context) =>  RegisterScreen(),
+          '/homeScreen': (BuildContext context) =>  HomeScreen(),
+         /* '/rec_dashboard': (BuildContext context) => const ReceiverDashboard(),*/
+        }
+
     );
   }
 }
@@ -47,20 +56,15 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    startTimer(Timer(
-        const Duration(seconds: 3),
-            () => Navigator.pushReplacement(context,
-            MaterialPageRoute(builder: (context) =>  LoginScreen())))
 
-    );
-
- //   );
+    startTimer();
 
   }
 
+
   @override
   Widget build(BuildContext context) {
-SizeConfig().init(context);
+    SizeConfig().init(context);
     return Scaffold(
 
       body: Column(
@@ -72,33 +76,37 @@ SizeConfig().init(context);
             children: [
               Container(
 
-                height: SizeConfig.screenHeight*0.3,
-                 decoration: BoxDecoration(
-                   color: CommonColor.splashContainer,
-                     borderRadius: BorderRadius.only(
-                     bottomRight: Radius.circular(70),bottomLeft: Radius.circular(70))
-                    ),
-                  ),
+                height: SizeConfig.screenHeight * 0.3,
+                decoration: BoxDecoration(
+                    color: CommonColor.splashContainer,
+                    borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(70),
+                        bottomLeft: Radius.circular(70))
+                ),
+              ),
 
               Padding(
-                padding:  EdgeInsets.only(top: SizeConfig.screenHeight*0.25),
+                padding: EdgeInsets.only(top: SizeConfig.screenHeight * 0.25),
                 child: Center(
                   child: Container(
-                      height: 100,
-                      width: 200,
-                      decoration: BoxDecoration(
+                    height: 100,
+                    width: 200,
+                    decoration: BoxDecoration(
                       color: Colors.white,
-                        shape: BoxShape.circle,
+                      shape: BoxShape.circle,
 
 
-                      ),child: Image(image: const AssetImage("assets/images/logo.png"),height:SizeConfig.screenHeight*0.1),
+                    ),
+                    child: Image(
+                        image: const AssetImage("assets/images/logo.png"),
+                        height: SizeConfig.screenHeight * 0.1),
 
                   ),
                 ),
               )
             ],
           ),
-          SplashScreen(SizeConfig.screenHeight,SizeConfig.screenWidth)
+          SplashScreen(SizeConfig.screenHeight, SizeConfig.screenWidth)
 
         ],
       ),
@@ -106,34 +114,36 @@ SizeConfig().init(context);
     );
   }
 
-   startTimer(context) async {}
 
   Widget SplashScreen(double parentHeight, double parentWidth) {
-    return  Column(
-     crossAxisAlignment: CrossAxisAlignment.center,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         Padding(
-          padding:  EdgeInsets.only(top: parentWidth*0.1),
-          child: Image(image: const AssetImage("assets/images/group.png"),height:parentHeight*0.2,),
+          padding: EdgeInsets.only(top: parentWidth * 0.1),
+          child: Image(image: AssetImage("assets/images/group.png"),
+              height: parentHeight * 0.2),
         ),
 
         SizedBox(height: 30),
 
-           Text("Anything on Rent",
-        style: TextStyle(
-        color: Colors.black,
-        fontSize: SizeConfig.blockSizeHorizontal*6.0,
-        fontWeight: FontWeight.bold,
-        ),
-        textAlign: TextAlign.center),
+        Text("Anything on Rent",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: SizeConfig.blockSizeHorizontal * 6.0,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center),
         SizedBox(height: 40),
         Padding(
-          padding:  EdgeInsets.only(left: parentWidth*0.09,right: parentWidth*0.09),
-          child: Text("Upgrade your Product with new products after 15 Days of use for free",
+          padding: EdgeInsets.only(
+              left: parentWidth * 0.09, right: parentWidth * 0.09),
+          child: Text(
+              "Upgrade your Product with new products after 15 Days of use for free",
               style: TextStyle(
                 color: Colors.black,
-                fontSize: SizeConfig.blockSizeHorizontal*4.0,
-             //     fontWeight: FontWeight.w400,
+                fontSize: SizeConfig.blockSizeHorizontal * 4.0,
+                //     fontWeight: FontWeight.w400,
                 fontFamily: 'Montserrat-Medium',
 
               ),
@@ -142,12 +152,46 @@ SizeConfig().init(context);
         ),
 
 
-           //     Style
+        //     Style
 
       ],
     );
   }
 
 
- 
+  void navigateParentPage() {
+    Navigator.of(context).pushReplacementNamed('/frame');
+  }
+  void navigateRegistration() {
+    Navigator.of(context).pushReplacementNamed('/registrations');
+  }
+
+  void navigateHomePage() {
+    Navigator.of(context).pushReplacementNamed('/homeScreen');
+  }
+
+
+
+     startTimer() {
+      var duration = const Duration(seconds: 3);
+      try {
+        String accessToken = GetStorage().read(ConstantData.UserAccessToken);
+
+        print("-----> $accessToken");
+
+        if (accessToken.isEmpty) {
+          return Timer(duration, navigateParentPage);
+        } else if (accessToken.isNotEmpty) {
+          return Timer(duration, navigateHomePage);
+        }
+      } catch (e) {
+
+        print("eeeeeeee  $e");
+      }
+      return Timer(duration, navigateParentPage);
+    }
+
+
+
+
 }
